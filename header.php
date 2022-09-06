ccfbc2ddb2e2aa7b412d93cec5f203dbc0c4778a
Muhammad-Dzaki-Arrahman <78633671+Muhammad-Dzaki-Arrahman@users.noreply.github.com>
2022-09-06 21:05:00 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Header</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Web Lanjut</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="http://localhost/codeigniter/intro/index">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/codeigniter/contoh/index">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://localhost/codeigniter/mahasiswa/surel">Mahasiswa</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+</body>
+</html>
