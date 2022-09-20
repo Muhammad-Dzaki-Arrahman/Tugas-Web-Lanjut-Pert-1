dc251fb387a325bd46da6d998d5dc9108ec5dba6
Muhammad-Dzaki-Arrahman <78633671+Muhammad-Dzaki-Arrahman@users.noreply.github.com>
2022-09-20 19:27:36 +0700

    Add files via upload

--- app/Views/pages/mahasiswa.php
+++ app/Views/pages/mahasiswa.php
@@ -8,6 +8,8 @@
       <th scope="col">Nama</th>
       <th scope="col">Alamat</th>
       <th scope="col">Create At</th>
+      <th scope="col">Update At</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
@@ -19,6 +21,16 @@
                 <td><?= $mhs['nama'] ?></td>
                 <td><?= $mhs['alamat'] ?></td>
                 <td><?= $mhs['create_at'] ?></td>
+                <td><?= $mhs['updated_at'] ?></td>
+                <td>
+                  <div>
+                    <a class="btn btn-warning mr-3" href="/edit/<?= $mhs['id'] ?>">Edit</a>
+                    <form action="/delete/<?= $mhs['id'] ?>" method="post">
+                      <input name="_method" value="DELETE" type="hidden">
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                  </div>
+                </td>
           </tr>
         <?php $no++; ?>
       <?php endforeach; ?>
