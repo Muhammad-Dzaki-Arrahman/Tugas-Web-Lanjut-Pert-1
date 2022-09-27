23b12e4c4878f1f35fc6dbfa6c7cfbe516a456dc
Muhammad-Dzaki-Arrahman <78633671+Muhammad-Dzaki-Arrahman@users.noreply.github.com>
2022-09-27 22:39:03 +0700

    Update about.php

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,1 +1,5 @@
+<!-- gunakan fungsi extend(),section(),dan endSection() untuk aktivasi template yang sudah didownload  -->
+<?= $this->extend('template')?>
+<?= $this->section('content')?>
 <h4>This is About Page</h4>
+<?= $this->endSection() ?>
