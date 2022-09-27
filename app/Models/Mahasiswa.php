56f2d5193dbb2892f0c5e8a6f8cebaaaa2357d1e
Muhammad-Dzaki-Arrahman <78633671+Muhammad-Dzaki-Arrahman@users.noreply.github.com>
2022-09-27 22:41:25 +0700

    Update Mahasiswa.php

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -14,12 +14,12 @@ class Mahasiswa extends Model
     // protected $returnType       = 'array';
     // protected $useSoftDeletes   = false;
     // protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['npm', 'nama', 'alamat', 'deskripsi'];
 
     // Dates
     protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
+    protected $createdField  = 'create_at';
     protected $updatedField  = 'updated_at';
     // protected $deletedField  = 'deleted_at';
 
